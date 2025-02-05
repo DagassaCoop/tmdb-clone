a309cc3abf638c1d67b1d5180fa7b1f201d9de6f
Dmytro Bondar <dagassa@Dmytros-Laptop.local>
2025-02-05 17:59:31 -0500

    update: new structure; add: new api respond type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,3 @@
-export enum MoviesList {
-  POPULAR = "popular",
-  NOW_PLAYING = "now_playing",
-  UPCOMING = "upcoming",
-  TOP_RATED = "top_rated",
-}
-
-export enum TVShowsList {
-  POPULAR = "popular",
-  AIRING_TODAY = "airing_today",
-  ON_TV = "on_the_air",
-  TOP_RATED = "top_rated",
-}
+export * from "./movies";
+export * from "./tv";
+export * from "./api";
---
+++ src/types/tv.ts
@@ -0,0 +1,6 @@
+export enum TVShowsList {
+  POPULAR = "popular",
+  AIRING_TODAY = "airing_today",
+  ON_TV = "on_the_air",
+  TOP_RATED = "top_rated",
+}
