04b8e7f419be6dfb41387e79d20f6739e3e8fdeb
Dagassa <dagassacoop@gmail.com>
2025-04-14 19:05:03 -0400

    add: mock of movie lists into discover format with filter configs

--- src/api/tmdb/mock/discover.ts
+++ src/api/tmdb/mock/discover.ts
@@ -1,14 +1,52 @@
 // Types
 import { MoviesList } from "@/types";
 
-export const moviesFilters = {
-  [MoviesList.POPULAR]: [
-    {
-      include_adult: false,
-      include_video: false,
-      language: "en-US",
-      page: 1,
-      sort_by: "popularity.desc",
-    },
-  ],
+export const filterPresets = {
+  movie: {
+    [MoviesList.POPULAR]: [
+      {
+        include_adult: false,
+        include_video: false,
+        language: "en-US",
+        page: 1,
+        sort_by: "popularity.desc",
+      },
+    ],
+    [MoviesList.NOW_PLAYING]: [
+      {
+        include_adult: false,
+        include_video: false,
+        language: "en-US",
+        page: 1,
+        sort_by: "popularity.desc",
+        with_release_type: "2|3",
+        release_date_gte: "{min_date}",
+        release_date_lte: "{max_date}",
+      },
+    ],
+    [MoviesList.TOP_RATED]: [
+      {
+        include_adult: false,
+        include_video: false,
+        language: "en-US",
+        page: 1,
+        sort_by: "vote_average.desc",
+        without_genres: "99,10755",
+        vote_count_gte: 200,
+      },
+    ],
+    [MoviesList.UPCOMING]: [
+      {
+        include_adult: false,
+        include_video: false,
+        language: "en-US",
+        page: 1,
+        sort_by: "popularity.desc",
+        with_release_type: "2|3",
+        release_date_gte: "{min_date}",
+        release_date_lte: "{max_date}",
+      },
+    ],
+  },
+  tv: {},
 };
